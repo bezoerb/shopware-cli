5a5f72720d2b8e359822d07bc22d648e88483a79
Ben Zörb <ben@sommerlaune.com>
2017-08-02 17:08:13 +0200

    Check for existing installation

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ const meow = require('meow');
 const updateNotifier = require('update-notifier');
 const logSymbols = require('log-symbols');
 const trimNewlines = require('trim-newlines');
+const chalk = require('chalk');
 const redent = require('redent');
 const ui = require('./lib/ui');
 const {base} = require('./lib/env');
@@ -22,21 +23,30 @@ const rcContent = `# This file is required to identify the root installation dir
 # The config can be found in: ${store.path}`;
 
 const showHelp = () => console.log(redent(trimNewlines(`
-	Usage
-	  $ shopware <command>
-	  
-	Commands
-	  install  Setup new shopware installation
-	  refresh  Refresh shopware installation and plugins
-	  console  Make calls to the shopware console from anywhere in your project
-	  phar     Make calls to the shopware sw.phar from anywhere in your project
-	   
-	Options
-	  --tbd       
-	Examples
-	  $ swag install
-	  $ swag 
+  Usage
+    $ shopware <command>
+    
+  Commands
+    install  Setup new shopware installation
+    refresh  Refresh shopware installation and plugins from git
+    console  Make calls to the shopware console from anywhere in your project
+    phar     Make calls to the shopware sw.phar from anywhere in your project
+    help     Show this help
+     
+  Options
+    --tbd 
+          
+  Examples
+    $ shopware install
+    $ shopware 
 `), 2));
+
+// check existing installation
+const initial = ![
+  path.join(base, '.shopware-cli.json'),
+  path.join(base, 'src','config.php'),
+  path.join(base, 'src','shopware.php'),
+].reduce((res, file) => res || fs.existsSync(file), false);
 
 Promise
   .resolve()
@@ -44,12 +54,17 @@ Promise
   .then(({cmd, input}) => {
     if (cmd === 'install') {
       fs.writeFileSync(path.join(base, '.shopware-cli.json'), rcContent);
-      return ui(cli.flags).then(answers => ({cmd, answers, input}));
+      return ui.questions(Object.assign({},cli.flags, {initial})).then(answers => ({cmd, answers, input}));
     }
 
     return {cmd, answers: {}, input};
   })
   .then(({cmd, answers, input}) => {
+    if (!answers.overwrite && !initial) {
+      console.log(redent(trimNewlines(`${chalk.red('Canceled due to existing installation')}`)));
+      process.exit(0);
+    }
+    
     if (cmd) {
       return swag(cmd, input, Object.assign({}, cli.flags, answers));
     }
