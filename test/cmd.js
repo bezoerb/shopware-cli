d56826ee31fb759bec0260ca96f40fda626b55eb
Ben Zörb <ben@sommerlaune.com>
2017-09-01 05:51:33 +0200

    Update test

--- test/cmd.js
+++ test/cmd.js
@@ -1,16 +1,5 @@
-import os from 'os';
-import fs from 'fs-extra';
-import path from 'path';
 import test from 'ava';
-import makeDir from 'make-dir';
 import swag from '..';
-
-const testdir = path.join(os.tmpdir(), 'shopware-cli-test');
-
-test.before('prepare', async () => {
-  await makeDir(testdir);
-  process.chdir(testdir);
-});
 
 test('fail "console" without swdir', async t => {
   await t.throws(swag('console'));
@@ -18,27 +7,4 @@ test('fail "console" without swdir', async t => {
 
 test('fail "tools" without swdir', async t => {
   await t.throws(swag('tools'));
-});
-
-test('install', async t => {
-  await swag('install', {}, {
-    url: 'localhost',
-    dbname: 'shopware_test',
-    dbuser: 'root',
-    dbpass: '',
-    dbhost: '127.0.0.1',
-    dbport: '3306'
-  });
-
-  t.true(fs.existsSync(path.join(testdir, 'src/config.php')), 'shopware config generated');
-
-  const contents = await fs.readFile(path.join(testdir, 'src/config.php'), 'utf8');
-
-  t.regex(contents, new RegExp(`'username'\s+=>\s+'root'`)); // eslint-disable-line no-useless-escape
-  t.regex(contents, new RegExp(`'password'\s+=>\s+'breozb'`)); // eslint-disable-line no-useless-escape
-  t.regex(contents, new RegExp(`'dbname'\s+=>\s+'satisfyr'`)); // eslint-disable-line no-useless-escape
-  t.regex(contents, new RegExp(`'host'\s+=>\s+'127.0.0.1'`)); // eslint-disable-line no-useless-escape
-  t.regex(contents, new RegExp(`'port'\s+=>\s+'3306'`)); // eslint-disable-line no-useless-escape
-
-  t.pass(`Done in ${process.cwd()}`);
 });
---
+++ test/install.js
@@ -0,0 +1,38 @@
+import os from 'os';
+import path from 'path';
+import fs from 'fs-extra';
+import test from 'ava';
+import makeDir from 'make-dir';
+import uuid from 'uuid/v4';
+import swag from '..';
+
+test.beforeEach('prepare', async () => {
+  const cwd = path.join(os.tmpdir(), 'shopware-cli-test', uuid());
+  await makeDir(cwd);
+  process.chdir(cwd);
+
+  await swag('install', {}, {
+    url: 'localhost',
+    dbname: 'shopware_test',
+    dbuser: 'root',
+    dbpass: '',
+    dbhost: '127.0.0.1',
+    dbport: '3306'
+  });
+
+  t.context.cwd = cwd;
+});
+
+test('install', async t => {
+  t.true(fs.existsSync(path.join(t.context.cwd, 'src/config.php')), 'shopware config generated');
+
+  const contents = await fs.readFile(path.join(t.context.cwd, 'src/config.php'), 'utf8');
+
+  t.regex(contents, new RegExp(`'username'\\s+=>\\s+'root'`));
+  t.regex(contents, new RegExp(`'password'\\s+=>\\s+'breozb'`));
+  t.regex(contents, new RegExp(`'dbname'\\s+=>\\s+'satisfyr'`));
+  t.regex(contents, new RegExp(`'host'\\s+=>\\s+'127.0.0.1'`));
+  t.regex(contents, new RegExp(`'port'\\s+=>\\s+'3306'`));
+
+  t.pass(`Done in ${process.cwd()}`);
+});
